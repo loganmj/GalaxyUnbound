ffdc696b0f2237531920268eb71448b2dc839d02
Michael Logan <michaellogan7860@gmail.com>
2022-09-06 14:46:12 -0500

    Added the skeleton for the Army object.

---
+++ GalaxyUnbound/ArmyObjects/Army.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class Army
+{
+    #region Private Data
+
+    private List<CombatUnit> _units;
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// The list of all of the Combat Units that make up this army.
+    /// </summary>
+    public List<CombatUnit> Units => _units;
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// Base constructor
+    /// </summary>
+    public Army()
+    {
+        // TODO
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Adds a Combat Unit to the Army.
+    /// </summary>
+    /// <param name="unit"></param>
+    public AddUnit(CombatUnit unit) 
+    {
+        // TODO
+    }
+
+    /// <summary>
+    /// Removes a Combat Unit from the Army.
+    /// </summary>
+    /// <param name="unit"></param>
+    public RemoveUnit(CombatUnit unit) 
+    {
+        // TODO
+    }
+
+    #endregion
+}
