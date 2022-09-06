1e4e13cff6ba9816bb2afe85a2a24dc2f2f4e2ac
loganmj <53303258+loganmj@users.noreply.github.com>
2022-09-06 15:42:39 -0500

    Dev (#10)
    * Added empty Game Rules document.
    
    * Added the title page, table of contents, and a few starting sections.
    
    * Added "Army", "Combat Unit", and "Member" terms to the Glossary.
    
    * Added a bunch of stuff.
    
    * added game rules
    
    * Added the skeleton of the CombatUnit object. (#6)
    
    * Add combat unit (#7)
    
    * Added the skeleton of the CombatUnit object.
    
    * Added the skeleton for the Army object.
    
    * Fixed a couple errors with the Army class.
    
    * Converted the properties in the CombatUnit to full Properties.
    
    * Added the Member class. Renamed "ArmyObjects" folder to "UnitObjects".
    
    * Combat unit (#9)
    
    * Added Members list to the Combat Unit class.
    
    * Added Factions and UnitTypes enums to CombatUnits.

---
+++ GalaxyUnbound/UnitObjects/CombatUnit.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+public class CombatUnit
+{
+    #region Enums
+
+    /// <summary>
+    /// The faction keywords
+    /// </summary>
+    public enum Factions
+    {
+        Blue,
+        Red,
+        Yellow
+    }
+
+    /// <summary>
+    /// The types of Combat Units
+    /// </summary>
+    public enum UnitTypes
+    {
+        Hero,
+        Core,
+        Elite,
+        Strike,
+        Heavy
+    }
+
+    #endregion
+
+    #region Private Data
+
+    private int _focusRemaining;
+    private int _focusMax;
+    private int _staminaRemaining;
+    private int _staminaMax;
+    private int _speed;
+    private int _agility;
+    private int _healthPoints;
+    private int _rangedSkill;
+    private int _meleeSkill;
+    private int _power;
+    private int _toughness;
+    private int _luck;
+    private List<Member> _members;
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets the current Focus value of the unit.
+    /// </summary>
+    public int FocusRemaining => _focusRemaining;
+
+    /// <summary>
+    /// Gets the maximum Focus value of the unit.
+    /// </summary>
+    public int FocusMax => _focusMax;
+
+    /// <summary>
+    /// Gets the current Stamina value of the unit.
+    /// </summary>
+    public int StaminaRemaining => _staminaRemaining;
+
+    /// <summary>
+    /// Gets the maximum Stamina value of the unit.
+    /// </summary>
+    public int StaminaMax => _staminaMax;
+
+    /// <summary>
+    /// Gets the Speed stat value of the unit.
+    /// </summary>
+    public int Speed => _speed;
+
+    /// <summary>
+    /// Gets the Agility stat value of the unit.
+    /// </summary>
+    public int Agility => _agility;
+
+    /// <summary>
+    /// Gets the Health Points stat value of the unit.
+    /// </summary>
+    public int HealthPoints => _healthPoints;
+
+    /// <summary>
+    /// Gets the Ranged Skill stat value of the unit.
+    /// </summary>
+    public int RangedSkill => _rangedSkill;
+
+    /// <summary>
+    /// Gets the Melee Skill stat value of the unit.
+    /// </summary>
+    public int MeleeSkill => _meleeSkill;
+
+    /// <summary>
+    /// Gets the Power stat value of the unit.
+    /// </summary>
+    public int Power => _power;
+
+    /// <summary>
+    /// Gets the Toughness stat value of the unit.
+    /// </summary>
+    public int Toughness => _toughness;
+
+    /// <summary>
+    /// Gets the Luck stat value of the unit.
+    /// </summary>
+    public int Luck => _luck;
+
+    /// <summary>
+    /// Gets the list of Members in this unit.
+    /// </summary>
+    public List<Member> Members => _members;
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// Base constructor
+    /// </summary>
+    public CombatUnit()
+    {
+        // TODO
+    }
+
+    #endregion
+}
---
+++ GalaxyUnbound/UnitObjects/Member.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Member
+{
+    #region Private Data
+
+    private int _name;
+    private int _healthRemaining;
+    private int _healthMax;
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// The name of the member.
+    /// </summary>
+    public int Name => _name;
+
+    /// <summary>
+    /// The amount of health points remaining for the member.
+    /// </summary>
+    public int HealthRemaining => _healthRemaining;
+
+    /// <summary>
+    /// The maximum health points available for the member.
+    /// </summary>
+    public int HealthMax => _healthMax;
+
+    #endregion
+
+    #region Constructor
+
+    /// <summary>
+    /// Base constructor
+    /// </summary>
+    public Member()
+	{
+		// TODO
+	}
+
+    #endregion
+}
